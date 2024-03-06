497fd9bf2441cfe5c2cfae1d25b1f3931795f2a6
Jisang Lee <matrixpower1004@gmail.com>
2024-03-06 18:15:30 +0900

    Feat: chapter 4 - functional interface

---
+++ src/main/java/org/chapter4/model/User.java
@@ -0,0 +1,25 @@
+package org.chapter4.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class User {
+
+    private int id;
+    private String name;
+
+    @Builder
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               '}';
+    }
+}
